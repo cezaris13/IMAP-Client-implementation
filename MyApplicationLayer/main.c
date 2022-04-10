69c1a0a9b119c960c23dcf3b5c221bbcf53f6839
cezaris13 <pijus.cezar13@gmail.com>
2022-04-10 15:14:37 +0300

    logs in, seg fault

--- MyApplicationLayer/main.c
+++ MyApplicationLayer/main.c
@@ -48,20 +48,81 @@ int initializeClient(char host[],char port[]){
   return socketId;
 }
 
+/* char *imap_recv(SSL *sslConnection, size_t size) { */
+/*   size_t cursor = 0; */
+/*   int rc; */
+
+/*   char *buffer = (char *)malloc(size * sizeof(char)); */
+/*   char *result = (char *)malloc(size * sizeof(char)); */
+/*   int read_blocked = 0; */
+/*   do{ */
+/*     read_blocked = 0; */
+/*     rc = (int)SSL_read(sslConnection, buffer, size); */
+/*     printf("None\n"); */
+/*     switch(SSL_get_error(sslConnection,rc)){ */
+/*       case SSL_ERROR_NONE: */
+/*         printf("None\n"); */
+/*         if (rc == -1) */
+/*           continue; */
+
+/*         buffer[rc] = '\0'; */
+/*         int len = (int)(sizeof(char)*(cursor++)*size) + rc; */
+/*         char *temp_str = buffer; */
+/*         char *temp_res = (char *)malloc(len); */
+/*         memcpy(temp_res, result, len); */
+
+/*         if (result != NULL) { */
+/*           strcat(temp_res, temp_str); */
+/*           memcpy(result, temp_res, strlen(temp_res)+1); */
+/*         } */
+/*         else { */
+/*           memcpy(result, buffer, strlen(buffer)+1); */
+/*         } */
+/*         if (rc < size) */
+/*           break; */
+/*         break; */
+/*       case SSL_ERROR_ZERO_RETURN: */
+/*         printf("zero return\n"); */
+/*         /\* End of data *\/ */
+/*         /\* if(!shutdown_wait) *\/ */
+/*         /\*   SSL_shutdown(ssl); *\/ */
+/*         /\* goto end; *\/ */
+/*         break; */
+/*       case SSL_ERROR_WANT_READ: */
+/*         printf("want read\n"); */
+/*         read_blocked=1; */
+/*         break; */
+/*       case SSL_ERROR_WANT_WRITE: */
+/*         printf("want write\n"); */
+/*         /\* read_blocked_on_write=1; *\/ */
+/*         break; */
+/*       default: */
+/*         printf("default\n"); */
+/*         /\* berr_exit("SSL read problem"); *\/ */
+/*         break; */
+/*     } */
+/*   } */
+/*   while (SSL_pending(sslConnection) && !read_blocked); */
+
+/*   return result; */
+/* } */
 char *imap_recv(SSL *sslConnection, size_t size) {
   size_t cursor = 0;
   int rc;
 
-  char *buffer = (char *)malloc(size * sizeof(char));
-  char *result = (char *)malloc(size * sizeof(char));
-  while ((rc = (int)SSL_read(sslConnection, buffer, size))) {
+  char *buffer = malloc((size) * sizeof(char));
+  char *result = malloc((size) * sizeof(char));
+  while ((rc = SSL_read(sslConnection, buffer, size))) {
     if (rc == -1)
       continue;
 
+    printf("%d\n",rc);
     buffer[rc] = '\0';
-    int len = (int)(sizeof(char)*(cursor++)*size) + rc;
+    printf("buffer: %s\n",buffer);
+    int len = (int)(sizeof(char)*(cursor++)*(size)) + rc;
     char *temp_str = buffer;
-    char *temp_res = (char *)malloc(len);
+    char *temp_res = malloc(len);
+    printf("help\n");
     memcpy(temp_res, result, len);
 
     if (result != NULL) {
@@ -71,10 +132,12 @@ char *imap_recv(SSL *sslConnection, size_t size) {
     else {
       memcpy(result, buffer, strlen(buffer)+1);
     }
+    free(temp_res);
     if (rc < size)
         break;
   }
 
+  free(buffer);
   return result;
 }
 
@@ -130,18 +193,17 @@ int main(){
   SSL *sslConnection = ConnectSSL(fd);
   char *result=imap_recv(sslConnection, 100);
 
-  printf("%s\n",result);
-  char login[50];
-  snprintf(login, 50, "A2 LOGIN \"kt.testimap2022@gmail.com\" \"Q!w2e3r4t5\"\n");
+  char login[52];
+  snprintf(login, 52, "A2 LOGIN \"kt.testimap2022@gmail.com\" \"Q!w2e3r4t5\"\r\n");
 
   char *buffers[6] = {
-    "A1 CAPABILITY\n",
+    "A1 CAPABILITY\r\n",
     login,
-    "A3 CAPABILITY\n",
-    "A4 ID (\"name\" \"inbox\" \"version\" \"1.0.0\" \"support-url\" \"http://yorkiefixer.me\")\n",
-    "A5 SELECT \"INBOX\"\n",
-    //"A6 FETCH *:* (UID ENVELOPE)\n"
-    //"A7 LOGOUT\n"
+    "A3 CAPABILITY\r\n",
+    "A4 ID (\"name\" \"inbox\" \"version\" \"1.0.0\" \"support-url\" \"http://yorkiefixer.me\")\r\n",
+    "A5 SELECT \"INBOX\"\r\n",
+    //"A6 FETCH *:* (UID ENVELOPE)\r\n"
+    //"A7 LOGOUT\r\n"
   };
 
   int i = 0;
@@ -149,8 +211,7 @@ int main(){
     printf("S: %s\n", result);
     if (i == 0) {
       printf("C: %s\n", buffers[i]);
-      int a =SSL_write(sslConnection, buffers[i], strlen(buffers[i]));
-      printf("%d\n",a);
+      SSL_write(sslConnection, buffers[i], strlen(buffers[i]));
       i++;
       continue;
     }
@@ -161,8 +222,7 @@ int main(){
       }
       else {
         printf("C: %s\n", buffers[i]);
-        int a =SSL_write(sslConnection, buffers[i], strlen(buffers[i]));
-        printf("%d\n",a);
+        SSL_write(sslConnection, buffers[i], strlen(buffers[i]));
         i++;
       }
     }
