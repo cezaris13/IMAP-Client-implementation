7df70fbd9dbe3a587a296822a0478a55d4094302
cezaris13 <pijus.cezar13@gmail.com>
2022-04-20 15:15:20 +0300

    Several functions implemented

--- MyApplicationLayer/main.c
+++ MyApplicationLayer/main.c
@@ -14,6 +14,10 @@
 #include <openssl/ssl.h>
 #include <openssl/err.h>
 
+#define MAX_EMAIL_ADDRESS_LENGTH 320
+#define MAX_MAILBOX_NAME_SIZE 20
+int cursor=1;
+
 int initializeClient(char host[],char port[]){
   int socketId;
   struct addrinfo *servInfo, *currInfo, hints;
@@ -48,65 +52,6 @@ int initializeClient(char host[],char port[]){
   return socketId;
 }
 
-/* char *imap_recv(SSL *sslConnection, size_t size) { */
-/*   size_t cursor = 0; */
-/*   int rc; */
-
-/*   char *buffer = (char *)malloc(size * sizeof(char)); */
-/*   char *result = (char *)malloc(size * sizeof(char)); */
-/*   int read_blocked = 0; */
-/*   do{ */
-/*     read_blocked = 0; */
-/*     rc = (int)SSL_read(sslConnection, buffer, size); */
-/*     printf("None\n"); */
-/*     switch(SSL_get_error(sslConnection,rc)){ */
-/*       case SSL_ERROR_NONE: */
-/*         printf("None\n"); */
-/*         if (rc == -1) */
-/*           continue; */
-
-/*         buffer[rc] = '\0'; */
-/*         int len = (int)(sizeof(char)*(cursor++)*size) + rc; */
-/*         char *temp_str = buffer; */
-/*         char *temp_res = (char *)malloc(len); */
-/*         memcpy(temp_res, result, len); */
-
-/*         if (result != NULL) { */
-/*           strcat(temp_res, temp_str); */
-/*           memcpy(result, temp_res, strlen(temp_res)+1); */
-/*         } */
-/*         else { */
-/*           memcpy(result, buffer, strlen(buffer)+1); */
-/*         } */
-/*         if (rc < size) */
-/*           break; */
-/*         break; */
-/*       case SSL_ERROR_ZERO_RETURN: */
-/*         printf("zero return\n"); */
-/*         /\* End of data *\/ */
-/*         /\* if(!shutdown_wait) *\/ */
-/*         /\*   SSL_shutdown(ssl); *\/ */
-/*         /\* goto end; *\/ */
-/*         break; */
-/*       case SSL_ERROR_WANT_READ: */
-/*         printf("want read\n"); */
-/*         read_blocked=1; */
-/*         break; */
-/*       case SSL_ERROR_WANT_WRITE: */
-/*         printf("want write\n"); */
-/*         /\* read_blocked_on_write=1; *\/ */
-/*         break; */
-/*       default: */
-/*         printf("default\n"); */
-/*         /\* berr_exit("SSL read problem"); *\/ */
-/*         break; */
-/*     } */
-/*   } */
-/*   while (SSL_pending(sslConnection) && !read_blocked); */
-
-/*   return result; */
-/* } */
-
 char *imap_recv(SSL *sslConnection, size_t size) {
   size_t cursor = 0;
   int rc;
@@ -140,7 +85,7 @@ char *imap_recv(SSL *sslConnection, size_t size) {
   return result;
 }
 
-int check_ok(char* str) {
+int check_ok(char* str) {//impove this function
   int len = (int)strlen(str);
   int is_ok = 0;
   for (int i=0; i<len; i++) {
@@ -180,33 +125,215 @@ void DestroySSL(){
     EVP_cleanup();
 }
 
-void SendAndReceiveImapMessage(char *command, SSL* sslConnection){
-   printf("C: %s\n", command);
-   SSL_write(sslConnection, command, strlen(command));
-   char *result = imap_recv(sslConnection, 100);
-   printf("S: %s\n", result);
-   free(result);
+int SendAndReceiveImapMessage(char *command, SSL* sslConnection){
+  printf("C: %s\n", command);
+  SSL_write(sslConnection, command, strlen(command));
+  char *response = imap_recv(sslConnection, 100);
+    int is_ok = check_ok(response);
+    printf("S: %s\n", response);
+    free(response);
+  if (is_ok) 
+    return 0;
+  else
+    return -1;
+}
+
+void CheckConnectionStatus(SSL *sslConnection){
+  char message[100];
+  snprintf(message, sizeof(message), "A%d CAPABILITY\r\n", cursor++);
+  if(SendAndReceiveImapMessage(message,sslConnection) == -1)
+    printf("Connection lost\n");
+}
+
+void LoginUser(SSL *sslConnection){
+  char *userName, *password;
+  userName = malloc(MAX_EMAIL_ADDRESS_LENGTH*sizeof(char));
+  password = malloc(MAX_EMAIL_ADDRESS_LENGTH*sizeof(char));
+  char message[MAX_EMAIL_ADDRESS_LENGTH*2+100];
+  printf("Enter your email address: ");
+  scanf("%s", userName);
+  printf("Enter your password: ");
+  scanf("%s", password);
+  snprintf(message, sizeof(message), "A%d LOGIN \"%s\" \"%s\"\r\n", cursor++, userName, password);
+  if(SendAndReceiveImapMessage(message,sslConnection) == -1)
+    printf("Login failed\n");
+  free(userName);
+  free(password);
+}
+  
+void LoginUserHardcoded(SSL *sslConnection){
+  char message[100];
+  snprintf(message, sizeof(message), "A%d LOGIN \"%s\" \"%s\"\r\n", cursor++, "kt.testimap2022@gmail.com", "Q!w2e3r4t5");
+  if(SendAndReceiveImapMessage(message,sslConnection) == -1)
+    printf("Login failed\n");
+}
+
+// void CheckInboxByUID(SSL *sslConnection){
+//   char message[100];
+//   snprintf(message, sizeof(message), "A%d SELECT INBOX\r\n", cursor++);
+//   if(SendAndReceiveImapMessage(message,sslConnection) == -1)
+//     printf("Select failed\n");
+//   snprintf(message, sizeof(message), "A%d UID SEARCH ALL\r\n", cursor++);
+//   if(SendAndReceiveImapMessage(message,sslConnection) == -1)
+//     printf("Search failed\n");
+// }
+
+// int UserIsLoggedIn(SSL *sslConnection){
+//   char message[100];
+//   snprintf(message, sizeof(message), "A%d CAPABILITY\r\n", cursor++);
+//   if(SendAndReceiveImapMessage(message,sslConnection) == -1)
+//     return 0;
+//   char *response = imap_recv(sslConnection, 100);
+//   int is_ok = check_ok(response);
+//   free(response);
+//   return is_ok;
+// }
+
+void LogoutUser(SSL *sslConnection){
+  char message[100];
+  snprintf(message, sizeof(message), "A%d LOGOUT\r\n", cursor++);
+  if(SendAndReceiveImapMessage(message,sslConnection) == -1)
+    printf("Logout failed\n");
+}
+
+// void SendEmail(SSL *sslConnection){
+//   char *recipient, *subject, *message;
+//   recipient = malloc(MAX_EMAIL_ADDRESS_LENGTH*sizeof(char));
+//   subject = malloc(MAX_EMAIL_ADDRESS_LENGTH*sizeof(char));
+//   message = malloc(MAX_EMAIL_ADDRESS_LENGTH*sizeof(char));
+//   printf("Enter recipient: ");
+//   scanf("%s", recipient);
+//   printf("Enter subject: ");
+//   scanf("%s", subject);
+//   printf("Enter message: ");
+//   scanf("%s", message);
+//   char message_to_send[MAX_EMAIL_ADDRESS_LENGTH*2+100];
+//   snprintf(message_to_send, sizeof(message_to_send), "A%d MAIL FROM: <%s>\r\n", cursor++, "
+//   snprintf(message_to_send, sizeof(message_to_send), "A%d RCPT TO: <%s>\r\n", cursor++, recipient);
+//   snprintf(message_to_send, sizeof(message_to_send), "A%d DATA\r\n", cursor++);
+//   snprintf(message_to_send, sizeof(message_to_send), "A%d FROM: <%s>\r\n", cursor++, "
+//   snprintf(message_to_send, sizeof(message_to_send), "A%d SUBJECT: %s\r\n", cursor++, subject);
+//   snprintf(message_to_send, sizeof(message_to_send), "A%d %s\r\n", cursor++, message);
+//   snprintf(message_to_send, sizeof(message_to_send), "A%d .\r\n", cursor++);
+//   if(SendAndReceiveImapMessage(message_to_send,sslConnection) == -1)
+//     printf("Send failed\n");
+//   free(recipient);
+//   free(subject);
+//   free(message);
+// }
+
+void SelectMailboxByName(SSL *sslConnection){
+  //check if logged in  
+  char message[100];
+  char *mailBoxName = malloc(MAX_MAILBOX_NAME_SIZE*sizeof(char));
+  printf("Enter mailbox name: ");
+  scanf("%s", mailBoxName);
+  snprintf(message, sizeof(message), "A%d SELECT \"%s\"\r\n", cursor++, mailBoxName);
+  if(SendAndReceiveImapMessage(message,sslConnection) == -1)
+    printf("Select failed\n");
+  free(mailBoxName);
+}
+
+void GetMailBoxes(SSL *sslConnection){
+  char message[100];
+  snprintf(message, sizeof(message), "A%d LIST \"\" \"*\"\r\n", cursor++);
+  if(SendAndReceiveImapMessage(message,sslConnection) == -1)
+    printf("List failed\n");
+}
+
+void CreateMailBox(SSL *sslConnection){
+  char message[100];
+  char *mailBoxName = malloc(MAX_MAILBOX_NAME_SIZE*sizeof(char));
+  printf("Enter mailbox name: ");
+  scanf("%s", mailBoxName);
+  if(strlen(mailBoxName) > MAX_MAILBOX_NAME_SIZE){
+    printf("Mailbox name is too long\n");
+    return;
+  }
+  snprintf(message, sizeof(message), "A%d CREATE \"%s\"\r\n", cursor++, mailBoxName);
+  if(SendAndReceiveImapMessage(message,sslConnection) == -1)
+    printf("Create failed\n");
+  free(mailBoxName);
+}
+
+void DeleteMailBox (SSL *sslConnection){
+  char message[100];
+  char *mailBoxName = malloc(MAX_MAILBOX_NAME_SIZE*sizeof(char));
+  printf("Enter mailbox name: ");
+  scanf("%s", mailBoxName);
+  if(strlen(mailBoxName) > MAX_MAILBOX_NAME_SIZE){
+    printf("Mailbox name is too long\n");
+    return;
+  }
+  snprintf(message, sizeof(message), "A%d DELETE \"%s\"\r\n", cursor++, mailBoxName);
+  if(SendAndReceiveImapMessage(message,sslConnection) == -1)
+    printf("Delete failed\n");
+  free(mailBoxName);
+}
+
+void RenameMailBox (SSL *sslConnection){
+  char message[100];
+  char *mailBoxName = malloc(MAX_MAILBOX_NAME_SIZE*sizeof(char));
+  char *newMailBoxName = malloc(MAX_MAILBOX_NAME_SIZE*sizeof(char));
+  printf("Enter mailbox name: ");
+  scanf("%s", mailBoxName);
+  if(strlen(mailBoxName) > MAX_MAILBOX_NAME_SIZE){
+    printf("Mailbox name is too long\n");
+    return;
+  }
+  printf("Enter new mailbox name: ");
+  scanf("%s", newMailBoxName);
+  if(strlen(newMailBoxName) > MAX_MAILBOX_NAME_SIZE){
+    printf("Mailbox name is too long\n");
+    return;
+  }
+  snprintf(message, sizeof(message), "A%d RENAME \"%s\" \"%s\"\r\n", cursor++, mailBoxName, newMailBoxName);
+  if(SendAndReceiveImapMessage(message,sslConnection) == -1)
+    printf("Rename failed\n");
+  free(mailBoxName);
+  free(newMailBoxName);
+}
+
+void noop (SSL *sslConnection){
+  char message[100];
+  snprintf(message, sizeof(message), "A%d NOOP\r\n", cursor++);
+  if(SendAndReceiveImapMessage(message,sslConnection) == -1)
+    printf("Noop failed\n");
+}
+
+void GetEmailCountForMailBox(SSL *sslConnection){
+  char message[100];
+  char *mailBoxName = malloc(MAX_MAILBOX_NAME_SIZE*sizeof(char));
+  printf("Enter mailbox name: ");
+  scanf("%s", mailBoxName);
+  if(strlen(mailBoxName) > MAX_MAILBOX_NAME_SIZE){
+    printf("Mailbox name is too long\n");
+    return;
+  }
+  snprintf(message, sizeof(message), "A%d STATUS \"%s\" (MESSAGES)\r\n", cursor++, mailBoxName);
+  if(SendAndReceiveImapMessage(message,sslConnection) == -1)
+    printf("Status failed\n");
+  free(mailBoxName);
 }
 
 void ShowImapCommands(SSL* sslConnection){
   int count = -1;
   int runProgram=1;
-  int cursor = 1;
   while(runProgram){
     char message[100];
 //should i process responses properly?
 //
-//noop?
-//get count
+//noop?+
+//get count+
 //search
 //select
 
 //login +
 //check response +
-//create mailbox
-//deletemailbox
-//rename mailbox
-//get mailboxes
+//create mailbox+
+//deletemailbox+
+//rename mailbox+
+//get mailboxes+
 //get unseen emails
 //get top emails
 //get mail
@@ -231,21 +358,20 @@ void ShowImapCommands(SSL* sslConnection){
            "13. create mailbox \n"
            "14. rename mailbox \n"
            "15. delete mailbox \n"
-           "16. logout \n"
-           "17. close system \n");
+           "16. get mailbox count \n"
+           "17. logout \n"
+           "18. close system \n");
     scanf("%d",&count);
     char mailbox[100];
     switch(count){
       case 1:
-        strcat(message, "CAPABILITY\r\n");
+        CheckConnectionStatus(sslConnection);
         break;
       case 2:
-        strcat(message,"LOGIN \"kt.testimap2022@gmail.com\" \"Q!w2e3r4t5\"\r\n");
+        LoginUser(sslConnection);
         break;
       case 3:
-        printf("enter mailbox\n");
-        scanf("%s",mailbox);
-        strcat(strcat(strcat(message,"SELECT \""),mailbox),"\"\r\n");
+        SelectMailboxByName(sslConnection);
         break;
       case 4:
       //take inbox name and get mail
@@ -276,26 +402,32 @@ void ShowImapCommands(SSL* sslConnection){
         runProgram=0;
         break;
       case 12:
-        strcat(message,"LIST \"\" *\r\n");// see how to format this
+        GetMailBoxes(sslConnection);
         break;
       case 13:
-        runProgram=0;
+        CreateMailBox(sslConnection);
         break;
       case 14:
-        runProgram=0;
+        RenameMailBox(sslConnection);
         break;
       case 15:
-        runProgram=0;
+        DeleteMailBox(sslConnection);
         break;
       case 16:
-        strcat(message,"LOGOUT\r\n");
+        GetEmailCountForMailBox(sslConnection);
+        break;
+      case 17:
+        LogoutUser(sslConnection);
+        break;
+      case 19:
+        LoginUserHardcoded(sslConnection);
+        break;
+      case 20:
+        noop(sslConnection);//what is this?
         break;
       default:
         runProgram=0;
         break;
-    }
-    if(runProgram){
-        SendAndReceiveImapMessage(message,sslConnection);
     }
   }
 }
@@ -316,32 +448,9 @@ int main(){
 
   /* char *buffers[6] = { */
   /*   "A4 ID (\"name\" \"inbox\" \"version\" \"1.0.0\" \"support-url\" \"http://yorkiefixer.me\")\r\n", */
-  /*   "A5 SELECT \"INBOX\"\r\n", */
   /*   //"A6 FETCH *:* (UID ENVELOPE)\r\n" */
 
   ShowImapCommands(sslConnection);
-  /* int i = 0; */
-  /* do { */
-  /*   printf("S: %s\n", result); */
-  /*   if (i == 0) { */
-  /*     printf("C: %s\n", buffers[i]); */
-  /*     SSL_write(sslConnection, buffers[i], strlen(buffers[i])); */
-  /*     i++; */
-  /*     continue; */
-  /*   } */
-
-  /*   if (check_ok(result)) { */
-  /*     if (i >= 5) { */
-  /*       break; */
-  /*     } */
-  /*     else { */
-  /*       printf("C: %s\n", buffers[i]); */
-  /*       SSL_write(sslConnection, buffers[i], strlen(buffers[i])); */
-  /*       i++; */
-  /*     } */
-  /*   } */
-  /* } */
-  /* while ((result = imap_recv(sslConnection, 100)) || 1); */
 
   SSL_free(sslConnection);
   DestroySSL();
